1cf83e2542844b513837a053e1cc59a2f41d12d7
Simon Ljus <simon.ljus94@gmail.com>
2024-10-16 13:39:34 +0200

    remove angular flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
@@ -48,7 +47,7 @@ import { environment } from './../environments/environment';
         BrowserModule,
         HttpClientModule,
         BrowserAnimationsModule,
-        FlexLayoutModule,
+  
 
         FormsModule,
         MaterialModule,
